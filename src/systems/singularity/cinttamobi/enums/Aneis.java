fb160878a210e5129ce347ebb44d6bf8e914c6e2
Pedro Tôrres <t0rr3sp3dr0@gmail.com>
2016-06-17 13:25:02 -0300

    Enums

---
+++ src/systems/singularity/cinttamobi/enums/Aneis.java
@@ -0,0 +1,32 @@
+package systems.singularity.cinttamobi.enums;
+
+/**
+ * Created by phts on 17/06/16.
+ */
+public enum Aneis {
+    A("A", 2.80),
+    B("B", 3.85),
+    D("D", 3.00),
+    G("G", 1.85);
+
+    private String name;
+    private double price;
+
+    Aneis(String name, double value) {
+        this.name = name;
+        this.price = value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
---
+++ src/systems/singularity/cinttamobi/negocio/Onibus.java
@@ -0,0 +1,7 @@
+package systems.singularity.cinttamobi.negocio;
+
+/**
+ * Created by phts on 17/06/16.
+ */
+public class Onibus {
+}
