1a3ff815557262d77688c2847c1e41036d0d3d11
esvm <esvm@cin.ufpe.br>
2016-06-17 10:07:07 -0300

    Trabalhador + VEM trabalhador

---
+++ src/systems/singularity/cinttamobi/util/Trabalhador.java
@@ -0,0 +1,12 @@
+package systems.singularity.cinttamobi.util;
+
+import java.util.Date;
+
+/**
+ * Created by esvm on 17/06/16.
+ */
+public class Trabalhador extends Person {
+    public Trabalhador(String name, Date birth, String cpf) {
+        super(name, birth, cpf);
+    }
+}
---
+++ src/systems/singularity/cinttamobi/util/VEMTrabalhador.java
@@ -0,0 +1,23 @@
+package systems.singularity.cinttamobi.util;
+
+import systems.singularity.cinttamobi.util.abstracts.VEM;
+
+/**
+ * Created by esvm on 17/06/16.
+ */
+public class VEMTrabalhador extends VEM {
+
+    public VEMTrabalhador(String number, Person person) {
+        super(number, person);
+    }
+
+    @Override
+    public void credit(int value) {
+
+    }
+
+    @Override
+    public void debit(int value) {
+
+    }
+}
