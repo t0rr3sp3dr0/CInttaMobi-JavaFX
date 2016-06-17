1eba32d64dee342907adfaf7ec4b94a7bd3ecb9f
esvm <esvm@cin.ufpe.br>
2016-06-17 09:20:43 -0300

    Repositórios VEM

---
+++ src/systems/singularity/cinttamobi/util/Repositories/RepositorioVEMLista.java
@@ -0,0 +1,41 @@
+package systems.singularity.cinttamobi.util.Repositories;
+
+import systems.singularity.cinttamobi.interfaces.Repository;
+
+import java.sql.SQLException;
+import java.util.List;
+
+/**
+ * Created by esvm on 17/06/16.
+ */
+public class RepositorioVEMLista implements Repository {
+    @Override
+    public boolean exists(Object object) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public void insert(Object object) throws SQLException, ClassNotFoundException {
+
+    }
+
+    @Override
+    public void update(Object object) throws SQLException, ClassNotFoundException {
+
+    }
+
+    @Override
+    public void remove(Object object) throws SQLException, ClassNotFoundException {
+
+    }
+
+    @Override
+    public List getAll() throws SQLException, ClassNotFoundException {
+        return null;
+    }
+
+    @Override
+    public Object search(Object object) throws SQLException, ClassNotFoundException {
+        return null;
+    }
+}
