cd757d6e97f5c2d4c628639349c8b9f6bf772db9
Lucas Valença <lvrma@cin.ufpe.br>
2016-06-19 14:26:32 -0300

    ArrayVEM completo

--- src/systems/singularity/cinttamobi/dados/vem/RepositorioArrayVEM.java
+++ src/systems/singularity/cinttamobi/dados/vem/RepositorioArrayVEM.java
@@ -2,17 +2,18 @@ package systems.singularity.cinttamobi.dados.vem;
 
 import systems.singularity.cinttamobi.abstracts.VEM;
 import systems.singularity.cinttamobi.interfaces.Repositorios;
+import systems.singularity.cinttamobi.negocio.ControladorVEM;
 
 /**
  * Created by lvrma on 19/06/16.
  */
-public class RepositorioArrayVEM implements Repositorios{
+public class RepositorioArrayVEM implements Repositorios {
 
     private VEM[] array = new VEM[0];
 
     @Override
     public boolean exists(String object) {
-        if(search(object) != null)
+        if (search(object) != null)
             return true;
         return false;
     }
@@ -20,9 +21,9 @@ public class RepositorioArrayVEM implements Repositorios{
     @Override
     public void insert(Object objectTemp) {
         VEM object = (VEM) objectTemp;
-        VEM[] temp = new VEM[array.length+1];
+        VEM[] temp = new VEM[array.length + 1];
 
-        for(int i = 0; i < array.length; i++)
+        for (int i = 0; i < array.length; i++)
             temp[i] = array[i];
 
         temp[array.length] = object;
@@ -32,20 +33,30 @@ public class RepositorioArrayVEM implements Repositorios{
     @Override
     public void update(Object objectTemp) {
         VEM object = (VEM) objectTemp;
-        for(int i = 0; i < array.length; i++)
-            if(array[i].getNumber().equals(object.getNumber()))
+        for (int i = 0; i < array.length; i++)
+            if (array[i].getNumber().equals(object.getNumber()))
                 array[i] = object;
     }
 
     @Override
-    public void remove(Object object) {
-        
+    public void remove(Object objectTemp) {
+        VEM object = (VEM) objectTemp;
+        VEM[] temp = new VEM[array.length - 1];
+
+        for (int i = 0; i < array.length; i++)
+            if (array[i].getNumber().equals(object.getNumber()))
+                array[i] = array[array.length - 1];
+
+        for (int i = 0; i < array.length - 1; i++)
+            temp[i] = array[i];
+
+        this.array = temp;
     }
 
     @Override
     public Object search(String object) {
-        for(int i = 0; i < array.length; i++)
-            if(array[i].getNumber().equals(object))
+        for (int i = 0; i < array.length; i++)
+            if (array[i].getNumber().equals(object))
                 return array[i];
         return null;
     }
