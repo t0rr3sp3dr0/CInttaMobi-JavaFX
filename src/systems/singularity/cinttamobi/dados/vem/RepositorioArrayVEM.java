704bf5cd9d965806eaef5f9a959be01820ef470b
Lucas Valença <lvrma@cin.ufpe.br>
2016-06-19 14:10:50 -0300

    Array incompleto

--- src/systems/singularity/cinttamobi/dados/vem/RepositorioArrayVEM.java
+++ src/systems/singularity/cinttamobi/dados/vem/RepositorioArrayVEM.java
@@ -1,5 +1,6 @@
 package systems.singularity.cinttamobi.dados.vem;
 
+import systems.singularity.cinttamobi.abstracts.VEM;
 import systems.singularity.cinttamobi.interfaces.Repositorios;
 
 /**
@@ -7,29 +8,45 @@ import systems.singularity.cinttamobi.interfaces.Repositorios;
  */
 public class RepositorioArrayVEM implements Repositorios{
 
+    private VEM[] array = new VEM[0];
 
     @Override
     public boolean exists(String object) {
+        if(search(object) != null)
+            return true;
         return false;
     }
 
     @Override
-    public void insert(Object object) {
+    public void insert(Object objectTemp) {
+        VEM object = (VEM) objectTemp;
+        VEM[] temp = new VEM[array.length+1];
 
+        for(int i = 0; i < array.length; i++)
+            temp[i] = array[i];
+
+        temp[array.length] = object;
+        array = temp;
     }
 
     @Override
-    public void update(Object object) {
-
+    public void update(Object objectTemp) {
+        VEM object = (VEM) objectTemp;
+        for(int i = 0; i < array.length; i++)
+            if(array[i].getNumber().equals(object.getNumber()))
+                array[i] = object;
     }
 
     @Override
     public void remove(Object object) {
-
+        
     }
 
     @Override
     public Object search(String object) {
+        for(int i = 0; i < array.length; i++)
+            if(array[i].getNumber().equals(object))
+                return array[i];
         return null;
     }
 }
