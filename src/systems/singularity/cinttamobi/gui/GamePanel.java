59cf4bd74941f14f5cca10b16863460e42797f62
Lucas Valença <lvrma@cin.ufpe.br>
2016-06-20 17:13:04 -0300

    S W I N G

---
+++ src/systems/singularity/cinttamobi/gui/GamePanel.java
@@ -0,0 +1,61 @@
+package systems.singularity.cinttamobi.gui;
+
+import systems.singularity.cinttamobi.Programa;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+/**
+ * Created by lvrma on 20/06/16.
+ */
+public class GamePanel extends JPanel implements ActionListener, KeyListener, ComponentListener {
+
+    private int SizeW = 800;
+    private int SizeH = 600;
+    private int X = 0;
+    private int Y = 0;
+    private int ScaleW = 4;
+    private int ScaleH = 3;
+    private Image background = Toolkit.getDefaultToolkit().getImage(
+            Programa.class.getResource("/images/logo.png"));
+
+    public GamePanel(){
+        super();
+        this.addKeyListener(this);
+        this.addComponentListener(this);
+        this.setPreferredSize(new Dimension(SizeW, SizeH));
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        g.drawImage(background, 0, 0, getWidth(), getHeight(), this);
+    }
+
+    public void componentResized(ComponentEvent arg0) {
+        Rectangle present = arg0.getComponent().getBounds();
+        if(present.width == SizeW || present.width <= 0 || present.height <= 0 || present.height == SizeH){
+            setBounds(X, Y, SizeW, SizeH);
+            return;
+        }
+        setBounds(X, Y, present.width, present.width*ScaleH/ScaleW);
+        SizeH = getHeight();
+        SizeW = getWidth();
+        this.setPreferredSize(new Dimension(SizeW, SizeH));
+    }
+
+    public void componentShown(ComponentEvent arg0) {}
+
+    public void keyPressed(KeyEvent arg0) {}
+
+    public void keyReleased(KeyEvent arg0) {}
+
+    public void keyTyped(KeyEvent arg0) {}
+
+    public void actionPerformed(ActionEvent arg0) {}
+
+    public void componentHidden(ComponentEvent arg0) {}
+
+    public void componentMoved(ComponentEvent arg0) {}
+}
--- src/systems/singularity/cinttamobi/guiController/MainController.java
+++ src/systems/singularity/cinttamobi/guiController/MainController.java
@@ -4,8 +4,11 @@ import javafx.embed.swing.SwingNode;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
+import javafx.scene.layout.Pane;
+import systems.singularity.cinttamobi.gui.GamePanel;
 import systems.singularity.cinttamobi.negocio.gui.StageTools;
 
+import javax.swing.*;
 import java.io.IOException;
 import java.net.URL;
 import java.util.Properties;
@@ -28,9 +31,23 @@ public class MainController implements Initializable {
         }
         stageTools.setTabPane(tabPane);
 
+        setPane(swingNode);
+
+
         tabPane.setTabClosingPolicy(TabPane.TabClosingPolicy.SELECTED_TAB);
 
         stageTools.newTab("vemCadastro", tabPane);
         stageTools.newTab("onibusCadastro", tabPane);
     }
+
+    public void setPane(final SwingNode swingNode){
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                GamePanel panel = new GamePanel();
+                swingNode.setContent(panel);
+            }
+        });
+    }
+
 }
