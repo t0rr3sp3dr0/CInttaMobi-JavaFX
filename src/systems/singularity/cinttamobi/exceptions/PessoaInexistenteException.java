d195041ceb9bde8ae3d8b6b5bf3ed1bc0ae569f8
esvm <esvm@cin.ufpe.br>
2016-06-19 21:57:37 -0300

    Controlador Pessoa

---
+++ src/systems/singularity/cinttamobi/exceptions/PessoaInexistenteException.java
@@ -0,0 +1,10 @@
+package systems.singularity.cinttamobi.exceptions;
+
+/**
+ * Created by caesa on 19/06/2016.
+ */
+public class PessoaInexistenteException extends Exception {
+    public PessoaInexistenteException() {
+        super("Pessoa inexistente!");
+    }
+}
